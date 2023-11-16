c073939ec8bb70ee0255c7f1b7c0bf42a881e461
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-11-16 11:41:01 -0300

    refact: add transactional annotation on find all

--- src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -5,6 +5,7 @@ import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class GenreService {
     @Autowired
     private GenreRepository repository;
 
+    @Transactional(readOnly = true)
     public List<GenreDTO> findAll() {
         List<GenreDTO> result = repository.findAll()
                 .stream()
