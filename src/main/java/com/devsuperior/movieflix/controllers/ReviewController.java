9af7d991fef7ffe08f8ee118bd26f615152dae9b
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-11-17 11:50:09 -0300

    feat: insert review

--- src/main/java/com/devsuperior/movieflix/controllers/ReviewController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/ReviewController.java
@@ -1,9 +1,18 @@
 package com.devsuperior.movieflix.controllers;
 
+import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.services.ReviewService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/reviews")
@@ -11,5 +20,19 @@ public class ReviewController {
 
     @Autowired
     private ReviewService service;
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_MEMBER')")
+    public ResponseEntity<ReviewDTO> insert(@RequestBody @Valid ReviewDTO review) {
+        ReviewDTO result = service.insert(review);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(result.getId())
+                .toUri();
+        return ResponseEntity
+                .created(uri)
+                .body(result);
+    }
 
 }
--- src/main/java/com/devsuperior/movieflix/services/ReviewService.java
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.movieflix.services;
 
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReviewService {
@@ -10,4 +16,24 @@ public class ReviewService {
     @Autowired
     private ReviewRepository repository;
 
+    @Autowired
+    private AuthService authService;
+
+    @Transactional
+    public ReviewDTO insert(ReviewDTO review) {
+        Review save = new Review();
+        save.setText(review.getText());
+
+        save.setMovie(new Movie());
+        save.getMovie().setId(review.getMovieId());
+        save.setUser(authService.authenticated());
+
+        Long idSaved = repository.save(save).getId();
+        review.setId( idSaved );
+        review.setUserId(save.getUser().getId());
+        review.setUserName(save.getUser().getName());
+        review.setUserEmail(save.getUser().getEmail());
+
+        return review;
+    }
 }
